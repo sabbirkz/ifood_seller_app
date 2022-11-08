1a56455141ac5e2f47c1ac6a7fcacef3b487b245
sabbirkz <54450585+sabbirkz@users.noreply.github.com>
2022-11-08 17:10:16 +0600

    working on register part

--- lib/authentication/register.dart
+++ lib/authentication/register.dart
@@ -1,6 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:ifood_seller_app/widgets/custom_text_field.dart';
-
+import 'package:image_picker/image_picker.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
@@ -10,11 +12,48 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+
+
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  TextEditingController Controller = TextEditingController();
+  TextEditingController nameController = TextEditingController();
+  TextEditingController nameController = TextEditingController();
+
+  XFile? imageXFile;
+  final ImagePicker _picker = ImagePicker();
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: CustomTextField(
-        controller:
+    return SingleChildScrollView(
+      child: Container(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            SizedBox(
+              height: 10,
+            ),
+            InkWell(
+              child: CircleAvatar(
+                  radius: MediaQuery.of(context).size.width * 0.20,
+                  backgroundColor: Colors.white,
+                  backgroundImage: imageXFile == null
+                      ? null
+                      : FileImage(File(imageXFile!.path)),
+                  child: imageXFile == null
+                      ? Icon(
+                          Icons.add_photo_alternate,
+                          size: MediaQuery.of(context).size.width * 0.20,
+                          color: Colors.grey,
+                        )
+                      : null),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+          ],
+        ),
       ),
     );
   }
